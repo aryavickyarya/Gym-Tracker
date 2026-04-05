61522ffa5a5a3ab9725aba21dce1a6c085d2a2f3
aryavickyarya <aryavickyarya@gmail.com>
2026-04-06 00:53:20 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+﻿const CACHE_NAME = 'gym-tracker-v1';
+const ASSETS_TO_CACHE = [
+ './',
+ './index.html',
+ 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&family=Oswald:wght@600;700&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+ event.waitUntil(
+   caches.open(CACHE_NAME).then((cache) => {
+     return cache.addAll(ASSETS_TO_CACHE);
+   })
+ );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+ event.waitUntil(
+   caches.keys().then((cacheNames) => {
+     return Promise.all(
+       cacheNames.map((cache) => {
+         if (cache !== CACHE_NAME) {
+           return caches.delete(cache);
+         }
+       })
+     );
+   })
+ );
+});
+
+// Fetch Strategy: Network First, Fallback to Cache
+self.addEventListener('fetch', (event) => {
+ event.respondWith(
+   fetch(event.request).catch(() => {
+     return caches.match(event.request);
+   })
+ );
+});
